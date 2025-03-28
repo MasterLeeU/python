69e94a2eba06d60cd767a6d27386d237342aecf0
Master_LeeU <80079050+MasterLeeU@users.noreply.github.com>
2025-03-28 11:39:07 -0600

    Create Day6.py

---
+++ Day6.py
@@ -0,0 +1,13 @@
+#Functions, Code Blocks, and While Loops
+# Karel the Robot
+
+# Funtions
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+def dis_function():
+    print("hello")
+    print("bye")
+
+dis_function()
