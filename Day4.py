5e1b889420d0c105cab8c2ae236eb3e7987df10e
Master_LeeU <80079050+MasterLeeU@users.noreply.github.com>
2025-03-28 11:41:43 -0600

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -6,8 +6,21 @@ import random
 # print(random_integer)
 
 #Coin Flip
-coin_flip = random.randint(1, 2)
-if coin_flip == 1:
-    print("Heads")
-else:
-    print("Tails")
+# coin_flip = random.randint(1, 2)
+# if coin_flip == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+# Friend List Randomizer
+# friends = ["Lee", "Dean", "Francis", "Matt", "Remith"]
+# print(random.choice(friends))
+
+# random_index = random.randint(0, 4)
+# print(friends[random_index])
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
