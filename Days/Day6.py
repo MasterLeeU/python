3f3c13b95250e06e1b24eecccfb57233527fde85
Master_LeeU <80079050+MasterLeeU@users.noreply.github.com>
2025-03-29 03:20:35 +0000

    UpdateDay

--- Days/Day6.py
+++ Days/Day6.py
@@ -21,3 +21,24 @@ def dis_function():
 # Indentation should be done with spaces. 
 
 #While Loops
+while #something is true:
+    # do something
+    print("hello")
+    # this will run forever unless you break out of the loop
+    # to break out of the loop, you can use a break statement
+    # or you can use a condition to break out of the loop
+    # for example, if you want to break out of the loop after 10 iterations
+    count = 0
+    while count < 10:
+        print(count)
+        count += 1
+    # this will print the numbers from 0 to 9
+    # you can also use a while loop to create a countdown
+    # for example, if you want to create a countdown from 10 to 0
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    # this will print the numbers from 10 to 1
+    # you can also use a while loop to create a countdown
+    # for example, if you want to create a countdown from 10 to 0 
