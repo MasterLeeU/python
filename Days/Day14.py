06295a39835dc786337959915e0078f66b746f41
Master_LeeU <80079050+MasterLeeU@users.noreply.github.com>
2025-06-19 10:23:51 -0600

    Create Day14.py

---
+++ Days/Day14.py
@@ -0,0 +1,23 @@
+# Higher or Lower Game
+# Generate Artwork
+import random
+
+from art.py import logo
+print(logo)
+
+
+def format_data(account):
+    """Takes the account data and returns the printable format. """
+    account_name = account["name"]
+    account_desc = account["description"]
+    account_country = account["country"]
+
+
+# Clear the screen
+
+
+
+account_a = random.choice(data)
+account_b = random.choice(data)
+
+function
