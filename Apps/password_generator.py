8d4b9f177ed01667bac59985a783956b307ea4b6
Master_LeeU <80079050+MasterLeeU@users.noreply.github.com>
2025-06-26 07:19:57 -0600

    Update password_generator.py

--- Apps/password_generator.py
+++ Apps/password_generator.py
@@ -2,7 +2,7 @@
 import random
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n''o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z']
-symbols = ['`','~','!','@','#','$','%','^','&','*','(',')','-','=','_','+']
+symbols = ['`','~','!','@','#','$','%','^','&','*','(',')','-','=','_','+','\','|',']','}','[','{']
 
 print("Welcome to a Random Password Generator")
 num_letters = int(input("How many letters would you like in your password?\n"))
